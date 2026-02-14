0771df6a4b8706e5a3972c0d8b03039888bcf497
Muhammad-AIUB <mjubayer.aiub@gmail.com>
2026-02-14 14:32:58 +0600

    chore: remove obsolete SQL files and authentication API configuration

--- drop-index.sql
+++
@@ -1,4 +0,0 @@
--- Drop the problematic index
-DROP INDEX IF EXISTS "IDX_97672ac88f789774dd47f7c8be";
-
-
--- reset-db.sql
+++
@@ -1,6 +0,0 @@
--- Reset database schema (WARNING: This will delete all data!)
-DROP SCHEMA IF EXISTS public CASCADE;
-CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;
-
