c28015adcee78f491f92e4688238f70ab41a6d13
Muhammad-AIUB <mjubayer.aiub@gmail.com>
2026-02-28 08:00:02 +0600

    refactor: simplify imports in CreateWebinarRegistrationDto

--- src/modules/webinar-registrations/dto/create-webinar-registration.dto.ts
+++ src/modules/webinar-registrations/dto/create-webinar-registration.dto.ts
@@ -1,10 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  IsUUID,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class CreateWebinarRegistrationDto {
   @ApiProperty({ example: '550e8400-e29b-41d4-a716-446655440000' })
